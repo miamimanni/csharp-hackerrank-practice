ed8fd4d3628e88a378808cdd7094af4d124785e0
ManuelR <Manuel_Reyes@ultimatesoftware.com>
2018-06-20 15:25:03 -0400

    adding Angry_Professor

---
+++ Angry_Professor/Angry_Professor/AngryProfessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Angry_Professor
+{
+    public class AngryProfessor
+    {
+        /// <summary>
+        /// Determines if class is cancelled for angry profressor's class. If minimum threshold of students don't arrive to class in time, professor responds w/ Yes or No.
+        /// </summary>
+        /// <param name="minimumOntimeOrEarlyStudentsNeededForClass">integer value representing number of minimum students needed to attend class, otherwise class is cancelled</param>
+        /// <param name="studentArrivalTimes">integer array representing time student arrival. 0 is on time, less than 0 is early, after 0 is late.</param>
+        /// <returns>boolean value, true if class is cancelled, false if class is not cancelled</returns>
+        public static bool IsClassCancelled(int minimumOntimeOrEarlyStudentsNeededForClass, int[] studentArrivalTimes)
+        {
+            int countOfEarlyAndOnTimeStudents = 0;
+            foreach (int studentArrivalTime in studentArrivalTimes)
+            {
+                if (studentArrivalTime <= 0)
+                {
+                    countOfEarlyAndOnTimeStudents++;
+                    if (countOfEarlyAndOnTimeStudents == minimumOntimeOrEarlyStudentsNeededForClass)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int amountOfClasses = Convert.ToInt32(Console.ReadLine());
+
+            for (int classNumber = 0; classNumber < amountOfClasses; classNumber++)
+            {
+                int thresholdOfStudentsNeededForClass = Convert.ToInt32(Console.ReadLine().Split(' ')[1]);
+                int[] studentArrivalTimes = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+                bool result = IsClassCancelled(thresholdOfStudentsNeededForClass, studentArrivalTimes);
+                Console.WriteLine(result ? "YES" : "NO");
+            }
+        }
+    }
+}
---
+++ Angry_Professor/Angry_Professor_Tests/AngryProfressorTests.cs
@@ -0,0 +1,54 @@
+﻿using Angry_Professor;
+using NUnit.Framework;
+
+namespace Angry_Professor_Tests
+{
+    [TestFixture]
+    public class AngryProfressorTests
+    {
+        // An average cases
+        [TestCase(3, new[] { -1, 0, 1, 2 })]
+        // A few edge cases
+        [TestCase(1, new[] { 1 })]
+        [TestCase(2, new[] { 0, 1 })]
+        [TestCase(3, new[] { 0, 0, 1 })]
+        // HackerRank Sample Test Cases
+        [TestCase(3, new[] { -1, -3, 4, 2 })]
+        // Unusual case that is out of scope
+        [TestCase(int.MaxValue, new int[] {})]
+        public void AngryProfressor_GivenOnTimeStudentsAndThreshold_WhenOnTimeStudentsOutsideThresdhold_ThenProfessorCancelsClass(int thresholdOfStudentsNeededForClass, int[] studentArrivalTimes)
+        {
+            // Arrange
+            const bool expectedResult = true;
+
+            // Act
+            bool actualResult = AngryProfessor.IsClassCancelled(thresholdOfStudentsNeededForClass, studentArrivalTimes);
+
+            // Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        // An average cases
+        [TestCase(3, new[] { -2, -1, 0, 1, 2, 3, 4, 5 })]
+        // Edge Cases
+        [TestCase(1, new[] { 0 })]
+        [TestCase(2, new[] { 0, 0 })]
+        [TestCase(3, new[] { 0, 0, 0 })]
+        [TestCase(1, new[] { 0, 1 })]
+        [TestCase(2, new[] { 0, 0, 1 })]
+        [TestCase(3, new[] { 0, 0, 0, 1 })]
+        // Hackerrank Sample test cases
+        [TestCase(2, new[] { 0, -1, 2, 1 })]
+        public void AngryProfressor_GivenOnTimeStudentsAndThreshold_WhenOnTimeStudentsWithinThresdhold_ThenProfessorContinuesClass(int thresholdOfStudentsNeededForClass, int[] studentArrivalTimes)
+        {
+            // Arrange
+            const bool expectedResult = false;
+
+            // Act
+            bool actualResult = AngryProfessor.IsClassCancelled(thresholdOfStudentsNeededForClass, studentArrivalTimes);
+
+            // Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+    }
+}
